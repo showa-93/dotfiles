3bff477b0edb5e2d821e4183e4aa8e3f62363596
showa-93 <shumai.1215.2018@gmail.com>
2023-04-09 22:49:54 +0900

    fixed version

--- bin/go.sh
+++ bin/go.sh
@@ -5,7 +5,7 @@
 exists_package go
 
 if is_linux || is_wsl; then
-  version=1.19.4 &&
+  version=1.20.3 &&
     wget https://golang.org/dl/go${version}.linux-amd64.tar.gz &&
     sudo rm -rf /usr/local/go &&
     sudo tar -C /usr/local -xzf go${version}.linux-amd64.tar.gz &&

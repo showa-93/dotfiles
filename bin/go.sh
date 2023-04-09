98b3979a46e4aeaa3dd5d3199025b51bcf32a5cc
showa-93 <shumai.1215.2018@gmail.com>
2023-04-10 03:09:07 +0900

    goの最新バージョンをgithubから取得するように修正

--- bin/go.sh
+++ bin/go.sh
@@ -1,12 +1,51 @@
 #!/bin/bash
-. bin/util.sh
 # https://go.dev/doc/install
 
 exists_package go
 
+# Goの最新バージョンをとってくる
+version=1.0.0
+minor_version=0
+patch_version=0
+BK_IFS=$IFS
+IFS=, semantic=(${version//./,})
+i=0
+while : ; do
+  ((i++))
+  body=$(curl -s https://api.github.com/repos/golang/go/tags?page=${i})
+  IFS=${BK_IFS} versions=($(echo "$body" | jq -r .[].name))
+  if [ ${#versions[@]} -eq 0 ]; then
+    break
+  fi
+
+  for full_version in ${versions[@]}; do
+    cur_version=$(echo "$full_version" | sed -E 's/^go(1\.[0-9]{2}\.[0-9]{1,2})$/\1/')
+    if [ $cur_version == $full_version ]; then
+      continue
+    fi
+
+    IFS=, cur_semantic=(${cur_version//./,})
+    cur_minor=$(expr "${cur_semantic[1]}")
+    cur_patch=$(expr "${cur_semantic[2]}")
+    if [ $cur_minor -gt $minor_version ]; then
+      version=$cur_version
+      minor_version=$cur_minor
+      patch_version=$cur_patch
+      continue
+    fi
+    if [ $cur_minor -eq $minor_version ] && [ $cur_patch -gt $patch_version ]; then
+      version=$cur_version
+      minor_version=$cur_minor
+      patch_version=$cur_patch
+      continue
+    fi
+  done
+done
+
+echo v$version
+
 if is_linux || is_wsl; then
-  version=1.20.3 &&
-    wget https://golang.org/dl/go${version}.linux-amd64.tar.gz &&
+  wget https://golang.org/dl/go${version}.linux-amd64.tar.gz &&
     sudo rm -rf /usr/local/go &&
     sudo tar -C /usr/local -xzf go${version}.linux-amd64.tar.gz &&
     rm -r go${version}.linux-amd64.tar.gz
